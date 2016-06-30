bdc311e835d2b4b4fc9cbb647a9c4ad1eb76994d
JohnSmith <johnsmith1st@live.com>
2016-06-30 11:35:54 +0800

    impl connections api

--- src/routes/connections.js
+++ src/routes/connections.js
@@ -1,23 +1,29 @@
 'use strict';
 
-let router = require('express').router();
-let redisClientFromSession = require('../middleware/redis-client-from-session');
+let router = require('express').Router();
+let redisManager = require('../services/redis-manager');
+let getRedisClient = require('../middleware/get-redis-client');
 
 /**
- * Create redis connection and return session id.
+ * @route POST /connections
+ * @command AUTH
+ * @summary Create redis connection and return connection id.
  */
-router.post('/', (req, res, next) => {
-
+router.post('/', (req, res) => {
   let { host, port, auth } = req.body;
-
-
+  let r = redisManager.create(host, port, { auth_pass: auth });
+  res._jsonRedisResponse(r.id);
 });
 
 /**
- * Quit redis client and destroy connection.
+ * @route DELETE /connections
+ * @command QUIT
+ * @summary Quit redis client and destroy connection.
  */
-router.delete('/', redisClientFromSession, (req, res, next) => {
-
+router.delete('/', getRedisClient, (req, res) => {
+  let id = req.header('x-connection-id');
+  let d = redisManager.close(id);
+  res._jsonRedisResponse(d);
 });
 
 
